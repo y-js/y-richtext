df9286927ac1b8aa81ed547b12d50cd8d2eea436
Kevin Jahns <kevin.jahns@rwth-aachen.de>
2015-04-14 01:29:06 +0200

    Working version
    Still missing some things. But editing & selections (the hard part) is
    working now. The code could still need some refactoring.

--- examples/quilljs/index.js
+++ examples/quilljs/index.js
@@ -2,3 +2,21 @@ var quill = new Quill('#editor', {
   theme: "snow"
 });
 quill.addModule('toolbar', { container: '#toolbar' });
+
+window.connector = new Y.XMPP().join("myprivatequill");
+connector.debug = true;
+window.y = new Y(connector);
+
+y.observe(function(events){
+  for(i in events){
+    if(events[i].name === "editor"){
+      y.val("editor").bind("QuillJs", quill);
+    }
+  }
+})
+
+connector.whenSynced(function(){
+  if(y.val("editor") == null){
+    y.val("editor", new Y.RichText())
+  }
+})
