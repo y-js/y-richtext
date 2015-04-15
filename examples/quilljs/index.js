1887752554faa0dc24eea46c5b285a483bcf9915
Corentin Cadiou <corentin.cadiou@linagora.com>
2015-04-15 16:17:05 +0200

    Use multi cursors in quill

--- examples/quilljs/index.js
+++ examples/quilljs/index.js
@@ -1,26 +1,27 @@
 var quill = new Quill('#editor', {
-    // modules: {
-    //     'toolbar': { container: '#toolbar' },
-    //     'link-tooltip': true
-    // },
+    modules: {
+        'multi-cursor': true,
+        'link-tooltip': true
+    },
     theme: 'snow'
 });
 quill.addModule('toolbar', { container: '#toolbar' });
-
-window.connector = new Y.XMPP().join('myprivatequill');
-connector.debug = true;
+window.connector = new Y.XMPP().join('sqfjqsmdlkjrhguemslkfjmlsdkjf');
+/* window.connector = new Y.WebRTC('sqfjqsmdlkjrhguemslkfjmlsdkjf',
+    {url: 'http://localhost:8888'}); */
+// connector.debug = true;
 window.y = new Y(connector);
 
-y.observe(function(events){
-    for(i in events){
+y.observe (function (events) {
+    for (i in events){
         if(events[i].name === 'editor'){
             y.val('editor').bind('QuillJs', quill);
         }
     }
-})
+});
 
 connector.whenSynced(function(){
     if(y.val('editor') == null){
         y.val('editor', new Y.RichText());
     }
-})
+});
