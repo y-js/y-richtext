a3dbacbdee0298b3f82735d1988b7c614c0a4a4b
Corentin Cadiou <corentin.cadiou@linagora.com>
2015-04-29 11:55:22 +0200

    Notification wrapper

--- examples/quilljs/notifications.js
+++ examples/quilljs/notifications.js
@@ -1,3 +1,36 @@
 /**
  * Created by ccc on 4/28/15.
  */
+var notifyConnection = function (event, authors) {
+  'use strict';
+  var opts, message, authorName;
+  authorName = "An unknown user";
+  if (authors[event.user])
+    authorName = authors[event.user].name || authorName;
+
+  if (event.action === 'userLeft') {
+    message = authorName + ' joined';
+  } else if (event.action === 'userJoined') {
+    message = authorName + ' left';
+  } else {
+    console.log("Something weird happened", event);
+  }
+
+  opts = {
+    title: "Connections",
+    text: message,
+    addclass: "stack-bottomright",
+    //stack: stack_bottomright,
+    nonblock: {
+      nonblock: true,
+      nonblock_opacity: 0.2
+    },
+    type: "info",
+    styling: 'bootstrap3'
+  };
+
+  new PNotify(opts);
+};
+
+window.notify = {};
+window.notify.connection = notifyConnection;
